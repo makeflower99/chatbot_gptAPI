f772212fea6ee0083cbd5979b80fd0b81914a804
dohyeonnlee <alsuswm2@gmail.com>
2024-04-23 15:56:45 +0900

    settings.py 다시수정

--- chatbot/chat/settings.py
+++ chatbot/chat/settings.py
@@ -130,9 +130,7 @@ USE_TZ = False
 STATIC_URL = "/static/"
 
 # 정적 파일 찾을 경로 설정
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
